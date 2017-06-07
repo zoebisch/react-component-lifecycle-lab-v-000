7f5a19b738d53247f126764c0c5fafa484e75434
Luke Ghenco <lukeghenco@gmail.com>
2017-06-07 05:34:33 -0400

    Add solution code

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,17 @@ class App extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: componentDidMount()
-  // TODO: componentWillUnmount()
+  componentWillMount() {
+    this.fetchTweets()
+  }
+
+  componentDidMount() {
+    this.startInterval();
+  }
+
+  componentWillUnmount() {
+    this.cleanUpInterval();
+  }
 
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,24 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    });
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.newTweets.length > 0;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [
+        ...nextProps.newTweets, 
+        ...this.state.tweets
+      ]
+    });
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
